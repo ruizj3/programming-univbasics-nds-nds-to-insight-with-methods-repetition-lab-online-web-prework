55566edb045858778c4541f7c18e2fbc69fe7a65
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-15 01:05:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,15 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  names = []
+  i = 0
+
+  while i < source.length do
+    names << source[i][:name]
+    i += 1
+  end
+
+  p names
 end
 
 def total_gross(source)
@@ -39,5 +48,3 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 end
-
-
